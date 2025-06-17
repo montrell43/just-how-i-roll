9281c9013057dd9cb29728700bb26b11959519f0
kylexrose@gmail.com <kylexrose@gmail.com>
2025-06-17 09:09:55 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+/**********
+ * DATA *
+ **********/
+
+const sixes = [];
+const doubleSixes = [];
+const twelves = [];
+const twenties = [];
+
+/********************
+ * HELPER FUNCTIONS *
+********************/
+
+const getRandomNumber = function(max) {
+    const rand = Math.random();
+    const range = rand * max;
+    const result = Math.ceil(range);
+    return result;
+}
+
+const sortByNumber = function(arr) {
+  const byNumber = function(item1, item2) {
+    return item1 - item2;
+  }
+
+  return arr.slice().sort(byNumber);
+}
+
+/*******************
+ * YOUR CODE BELOW *
+ *******************/
+
+
+
+/*******************
+ * EVENT LISTENERS *
+ *******************/
+
+
+
+
+
+/******************
+ * RESET FUNCTION *
+ ******************/
+
+
+
+/****************************
+ * CLICK HANDLING FUNCTIONS *
+****************************/
+
+
+
+/****************
+ * MATH SECTION *
+ ****************/
